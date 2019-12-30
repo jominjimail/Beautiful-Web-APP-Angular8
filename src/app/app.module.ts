e7c01ba55e0355ad56a48dbd6dcdff9beb0c8d88
jominjimail <jominjimail@gmail.com>
2019-12-30 20:57:22 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
